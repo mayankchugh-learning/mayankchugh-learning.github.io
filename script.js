e0d3227d29a7b187dd6453e4502c92992a3d0fde
mayankchugh-learning <mayankchugh.learning@gmail.com>
2024-04-18 09:47:04 +0800

    first version copied from Shivan profile

---
+++ script.js
@@ -0,0 +1,77 @@
+class TypingAnimation {
+  constructor(elementId, strings, typeSpeed = 70, backSpeed = 30, loop = true) {
+    this.typed = new Typed(elementId, {
+      strings: strings,
+      typeSpeed: typeSpeed,
+      backSpeed: backSpeed,
+      loop: loop,
+    });
+  }
+}
+
+class Counter {
+  constructor(selector, target, increment, interval) {
+    this.counterElement = document.querySelector(selector);
+    this.target = target;
+    this.increment = increment;
+    this.interval = interval;
+    this.count = 0;
+  }
+
+  startCounting() {
+    if (parseInt(this.counterElement.textContent) !== 0) {
+      return;
+    }
+
+    const duration = Math.floor(this.interval / this.target);
+    const counter = setInterval(() => {
+      this.count += this.increment;
+      this.counterElement.textContent = this.count;
+      if (this.count >= this.target) {
+        this.counterElement.textContent = `${this.count}+`;
+        clearInterval(counter);
+      }
+    }, duration);
+  }
+}
+
+function startCountersIfNeeded() {
+  const followersCounter = new Counter('#followers-text', 70000, 50, 4000);
+  const subscribersCounter = new Counter('#subscribers-text', 4000, 10, 4000);
+
+  followersCounter.startCounting();
+  subscribersCounter.startCounting();
+}
+
+function isInViewport(element) {
+  const rect = element.getBoundingClientRect();
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+}
+
+function handleScrollForCounters() {
+  const countersSection = document.querySelector('#stat-counter-section');
+  if (isInViewport(countersSection)) {
+    startCountersIfNeeded();
+    window.removeEventListener('scroll', handleScrollForCounters);
+  }
+}
+
+// Check if counters section is in viewport, if yes, start counting
+window.addEventListener('scroll', handleScrollForCounters);
+
+// Initialize typing animation
+const typingAnimation = new TypingAnimation("#typing-text", ["Kaggle Master", "Senior Data Scientist"]);
+
+window.addEventListener("scroll", function () {
+  const header = document.querySelector(".navbar");
+  if (window.scrollY > 250) {
+    header.classList.add("scrolled");
+  } else {
+    header.classList.remove("scrolled");
+  }
+});
